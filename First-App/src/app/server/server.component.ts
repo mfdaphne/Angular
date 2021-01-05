dc04d0f2f01885d4c7628d19de0666a446443450
Angular-App <fritzidaf@gmail.com>
2021-01-05 12:45:12 +0100

    String interpolation

--- First-App/src/app/server/server.component.ts
+++ First-App/src/app/server/server.component.ts
@@ -5,5 +5,10 @@ selector: 'app-server',
 templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
 
+    getServerStatus(){
+        return this.serverStatus;
+    }
 }
