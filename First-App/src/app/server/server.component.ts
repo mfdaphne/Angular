f0d657f1ee966a50225abd008878c87203b9fbe4
Angular-App <fritzidaf@gmail.com>
2021-01-06 10:50:44 +0100

    JustCommiting

--- First-App/src/app/server/server.component.ts
+++ First-App/src/app/server/server.component.ts
@@ -8,6 +8,10 @@ export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
+
     getServerStatus(){
         return this.serverStatus;
     }
