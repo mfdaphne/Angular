4380ce8e434941d8c32c7bcbb37442799c772186
Angular-App <fritzidaf@gmail.com>
2021-01-13 15:27:02 +0100

    global service added

--- cmp-service/src/app/app.module.ts
+++ cmp-service/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { FormsModule } from '@angular/forms';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- cmp-service/src/app/logging.service.ts
+++ cmp-service/src/app/logging.service.ts
@@ -1,6 +1,6 @@
 export class LoggingService {
 
     logStatusChange(status: string) {
-        console.log(status);
+        console.log('Status changed to : '+status);
     }
 }
