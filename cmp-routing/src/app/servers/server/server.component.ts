0abed80fa2c97a114b5cc9bc9c108f4dd29a8635
mfdaphne <fritzidaf@gmail.com>
2021-01-19 11:22:24 +0100

    child routes and query params

--- cmp-routing/src/app/servers/server/server.component.ts
+++ cmp-routing/src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -16,21 +16,23 @@ export class ServerComponent implements OnInit {
   };
 
   constructor(private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
-    let id = +this.route.snapshot.params['id'];
-    console.log('ID : ' + id);
-
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
 
     this.route.params.subscribe(
       (params: Params) => {
-        id = +params['id'];
+        this.server = this.serversService.getServer(+params['id']);
 
       }
     );
-    console.log('ID : ' + id)
-    this.server = this.serversService.getServer(id);
   }
 
+  onEdit() {
+    // this.router.navigate(['/servers', this.server.id, 'edit']);
+    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: "preserve" });
+  }
 }
