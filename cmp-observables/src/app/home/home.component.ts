f98cf84b5f1f06077e7617d8d8a488e23c7b096e
mfdaphne <fritzidaf@gmail.com>
2021-01-20 21:06:24 +0100

    custom observables

--- cmp-observables/src/app/home/home.component.ts
+++ cmp-observables/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  firstSubs: Subscription;
   constructor() { }
 
   ngOnInit() {
+    // this.firstSubs = interval(1000).subscribe(
+    //   (count) => {
+    //     console.log(count);
+    //   }
+    // );
+    const customCountObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000)
+    });
+
+    this.firstSubs = customCountObservable.subscribe(obs => {
+      console.log(obs);
+    });
   }
 
+  ngOnDestroy() {
+    this.firstSubs.unsubscribe();
+  }
 }
